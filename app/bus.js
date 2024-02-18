07d11a86cb61e0372a06cd62f40aac1808534fe1
Afnansayed <afnansayed1973@gmail.com>
2024-02-19 01:27:32 +0600

    java script

---
+++ app/bus.js
@@ -0,0 +1,73 @@
+function buyTicket() {
+    hide('information');
+    show('for-show');
+    show('seat-information');
+    getInnerText('left-seat');
+}
+
+const seatClass = document.getElementsByClassName('seat');
+for (const seat of seatClass) {
+    seat.addEventListener('click', function (event) {
+        //for left seat count
+        const allSeat = document.getElementById('left-seat');
+        const seatText = allSeat.childNodes[1].innerText;
+        const leftSeat = parseInt(seatText);
+        const currentSeat = leftSeat - 1;
+        //set current seat
+        allSeat.childNodes[1].innerText = currentSeat;
+        //user selected seat number
+        const seatContainer = document.getElementById('user-select').childNodes[1].childNodes[1].innerText;
+        const selectedSeatByUser = parseInt(seatContainer);
+        const updateSeatAmount = selectedSeatByUser + 1;
+        //set selected seat number
+        document.getElementById('user-select').childNodes[1].childNodes[1].innerText = updateSeatAmount;
+        if (updateSeatAmount === 4) {
+            show('coupon');
+        }
+        //show seat name
+        const showDtails = document.getElementById('amaount-container');
+        const p = document.createElement('p')
+        const p2 = document.createElement('p')
+        p2.innerText = "Ac_business"
+        const p3 = document.createElement('p')
+        //for price
+        const perTicket = getInnerText('per-ticket');
+        p3.innerText = perTicket + ' ' + 'Taka';
+        p.innerText = seat.innerText;
+        showDtails.appendChild(p);
+        showDtails.appendChild(p2);
+        showDtails.appendChild(p3);
+        //
+        const totalAmaount = getInnerText('total-amaount');
+        const total = totalAmaount + perTicket;
+        setInnerText('total-amaount', total)
+        //grand total
+        const grandAmount = getInnerText('grand-total');
+        const grandTotal = grandAmount + perTicket;
+        setInnerText('grand-total', grandTotal)
+
+        //coupon
+        const applyButton = document.getElementById('apply-button');
+        document.getElementById('coupon-input').addEventListener('keyup', function (event) {
+            const userPress = event.target.value;
+            if (userPress == 'NEW15' || userPress == 'Couple20') {
+                applyButton.classList.remove('hidden');
+            } else {
+                applyButton.classList.add('hidden', true);
+            }
+        })
+        
+        applyButton.addEventListener('click',function(){
+            const newYear = (grandTotal*15)/100;
+           const discounPrice = grandTotal - newYear;
+           console.log(discounPrice);
+           setInnerText('grand-total', discounPrice)
+           show('section-dis');
+         const discountContainer = document.getElementById('discount');
+         discountContainer.innerText = newYear;
+        })
+    })
+}
+
+
+
---
+++ app/uitility.js
@@ -0,0 +1,31 @@
+function hide(elementId){
+    document.getElementById(elementId).classList.add('hidden');
+}
+function show(elementId){
+    document.getElementById(elementId).classList.remove('hidden');
+}
+//add background color
+// function setBackgroundColor (elementId){
+//     document.getElementsByClassName(elementId).style.backgroundColor ='#1DD100';
+// }
+//get innertext
+function getInnerText(elementId){
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const value = parseInt(elementText);
+    return value;
+}
+function setInnerText(elementId,value){
+    const element = document.getElementById(elementId);
+     element.innerText = value;
+}
+
+
+// //
+// function setBackgroundColor(elementId) {
+//     // Get all elements with the specified class name
+//     const elements = document.getElementById(elementId);
+//    //const element = elements.innerText;
+//    console.log(elements);
+// }
+
